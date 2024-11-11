ca1b7d0af5fba169dea832ea158f7c164211149b
Ranganath2511 <150597821+Ranganath2511@users.noreply.github.com>
2024-11-11 14:14:23 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,7 +1,25 @@
-const decodeTheRing = function (s, p) {
+const decodeTheRing = function(s, p) {
+    function matchHelper(i, j) {
+        if (j === p.length) {
+            return i === s.length; 
+        }
 
-    // write your code here
+        
+        if (p[j] === '*') {
+           
+            return (i < s.length && matchHelper(i + 1, j)) || matchHelper(i, j + 1);
+        }
 
-  };
-  
-  module.exports = decodeTheRing;
+        if (i < s.length && (p[j] === '?' || p[j] === s[i])) {
+            return matchHelper(i + 1, j + 1);
+        }
+
+       
+        return false;
+    }
+
+   
+    return matchHelper(0, 0);
+};
+
+module.exports = decodeTheRing;
